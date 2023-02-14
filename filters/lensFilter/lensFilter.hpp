f9e5f52b1d319223c6f05108d1c2d882f06bd9be
Victor TOPORAN <victor.toporan@gmail.com>
2023-02-15 01:06:43 +0200

    Adds basic implementation for lens distortion filter

---
+++ filters/lensFilter/lensFilter.hpp
@@ -0,0 +1,20 @@
+#ifndef LENS_FILTER_H
+#define LENS_FILTER_H
+
+#include "genericFilter.hpp"
+#include <opencv2/core.hpp>
+
+class LensFitlerWrapper : public GenericFilterWrapper {
+  private:
+    float distortionAmount;
+
+  public:
+    explicit LensFitlerWrapper();
+    explicit LensFitlerWrapper(float initialAmount);
+
+    void applyFilter(cv::Mat& inframe);
+
+    void setdistortionAmount(int newAmount);
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "boxFilter.hpp"
 #include "cannyFilter.hpp"
 #include "dilateFilter.hpp"
-#include "embossFilter/embossFilter.hpp"
+#include "embossFilter.hpp"
 #include "erodeFilter.hpp"
 #include "gaussianFilter.hpp"
 #include "genericFilter.hpp"
+#include "lensFilter.hpp"
 #include "medianFilter.hpp"
 #include "sobelFilter.hpp"
-#include "embossFilter.hpp"
 
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
@@ -21,63 +21,65 @@ using namespace std;
 VideoCapture cap;
 
 void openCapture(int deviceID, int apiID) {
-    cap.open(deviceID, apiID);
-    if (!cap.isOpened()) {
-        cerr << "ERROR! Unable to open camera\n";
-        exit(-1);
-    }
+  cap.open(deviceID, apiID);
+  if (!cap.isOpened()) {
+    cerr << "ERROR! Unable to open camera\n";
+    exit(-1);
+  }
 }
 
-int main(int, char**) {
-    Mat inframe;
+int main(int, char **) {
+  Mat inframe;
 
-    int deviceID = 0;
-    int apiID = cv::CAP_ANY;
-    int ddepth = -1;
-    int kernel_size = 5;
-    Mat kernel = getStructuringElement(
-        MORPH_RECT, Size(2 * kernel_size + 1, 2 * kernel_size + 1),
-        Point(kernel_size, kernel_size));
+  int deviceID = 0;
+  int apiID = cv::CAP_ANY;
+  int ddepth = -1;
+  int kernel_size = 5;
+  Mat kernel = getStructuringElement(
+      MORPH_RECT, Size(2 * kernel_size + 1, 2 * kernel_size + 1),
+      Point(kernel_size, kernel_size));
 
-    openCapture(deviceID, apiID);
+  openCapture(deviceID, apiID);
 
-    MedianFitlerWrapper medianFilterWrapper(5);
-    BoxFitlerWrapper boxFilterWrapper(6);
-    GaussianFitlerWrapper gaussianFilterWrapper(15, 3, 30);
-    BilateralFitlerWrapper bilateralFilterWrapper(1, 1, 1);
-    ErodeFitlerWrapper erodeFilterWrapper(10);
-    DilateFitlerWrapper dilateFilterWrapper(10);
-    SobelFitlerWrapper sobelFilterWrapper(3, 1, 1);
-    CannyFitlerWrapper cannyFitlerWrapper(3, 15);
-    EmbossFitlerWrapper embossFitlerWrapper;
+  MedianFitlerWrapper medianFilterWrapper(5);
+  BoxFitlerWrapper boxFilterWrapper(6);
+  GaussianFitlerWrapper gaussianFilterWrapper(15, 3, 30);
+  BilateralFitlerWrapper bilateralFilterWrapper(1, 1, 1);
+  ErodeFitlerWrapper erodeFilterWrapper(10);
+  DilateFitlerWrapper dilateFilterWrapper(10);
+  SobelFitlerWrapper sobelFilterWrapper(3, 1, 1);
+  CannyFitlerWrapper cannyFitlerWrapper(3, 15);
+  EmbossFitlerWrapper embossFitlerWrapper;
+  LensFitlerWrapper lensFilterWrapper(3);
 
-    GenericFilterWrapper* wrappers[] = {
-        &medianFilterWrapper,   &boxFilterWrapper,
-        &gaussianFilterWrapper, &bilateralFilterWrapper,
-        &erodeFilterWrapper,    &dilateFilterWrapper,
-        &sobelFilterWrapper,    &cannyFitlerWrapper, &embossFitlerWrapper};
+  GenericFilterWrapper *wrappers[] = {
+      &medianFilterWrapper,    &boxFilterWrapper,   &gaussianFilterWrapper,
+      &bilateralFilterWrapper, &erodeFilterWrapper, &dilateFilterWrapper,
+      &sobelFilterWrapper,     &cannyFitlerWrapper, &embossFitlerWrapper,
+      &lensFilterWrapper,
+  };
 
-    sobelFilterWrapper.toggleDisplayDirection();
+  sobelFilterWrapper.toggleDisplayDirection();
 
-    bool keepGoing = true;
-    while (keepGoing) {
-        cap.read(inframe);
+  bool keepGoing = true;
+  do {
+    cap.read(inframe);
 
-        if (inframe.empty()) {
-            cerr << "ERROR! blank frame grabbed\n";
-            return -1;
-        }
-
-        int wrapperArrSize = sizeof(wrappers) / sizeof(wrappers[0]);
-        for (int i = wrapperArrSize - 1; i < wrapperArrSize; ++i) {
-            cv::Mat outframe = inframe.clone();
-            wrappers[i]->applyFilter(outframe);
-            imshow("out" + to_string(i), outframe);
-            moveWindow("out" + to_string(i), (i / 2) * 400, (i % 2) * 500);
-        }
-        if(waitKey(5) == ' ') {
-            keepGoing = false;
-        }
+    if (inframe.empty()) {
+      cerr << "ERROR! blank frame grabbed\n";
+      return -1;
     }
-    return 0;
+
+    int wrapperArrSize = sizeof(wrappers) / sizeof(wrappers[0]);
+    for (int i = wrapperArrSize - 1; i < wrapperArrSize; ++i) {
+      cv::Mat outframe = inframe.clone();
+      wrappers[i]->applyFilter(outframe);
+      imshow("out" + to_string(i), outframe);
+      moveWindow("out" + to_string(i), (i / 2) * 400, (i % 2) * 500);
+    }
+    if (waitKey(5) == ' ') {
+      keepGoing = false;
+    }
+  } while (keepGoing);
+  return 0;
 }

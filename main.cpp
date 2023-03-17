d6c0a21abd489b183c19d6c52375b2599f3eaae0
Victor TOPORAN <victor.toporan@gmail.com>
2023-03-17 20:03:13 +0200

    Adds Qt support

--- main.cpp
+++ main.cpp
@@ -33,61 +33,24 @@ void openCapture() {
     }
 }
 
-int wrapperIndex = 0;
-bool freezeFrame = false;
+int wrapperIndex = 5;
+int freezeFrame = false;
 void CallBackFunc(int event, int x, int y, int flags, void *userdata) {
     if (event == cv::EVENT_LBUTTONDOWN) {
         freezeFrame = !freezeFrame;
     }
 }
 
-int main(int, char **) {
-    cv::Mat inframe;
+#include <QApplication>
+#include <QLabel>
+#include <QWidget>
 
-    openCapture();
+int main(int argc, char *argv[]) {
 
-    MedianFitlerWrapper medianFilterWrapper(5);
-    BoxFitlerWrapper boxFilterWrapper(9);
-    GaussianFitlerWrapper gaussianFilterWrapper(15, 3, 30);
-    BilateralFitlerWrapper bilateralFilterWrapper(3, 3, 3);
-    ErodeFitlerWrapper erodeFilterWrapper(10);
-    DilateFitlerWrapper dilateFilterWrapper(10);
-    SobelFitlerWrapper sobelFilterWrapper(3, 1, 1);
-    CannyFitlerWrapper cannyFitlerWrapper(3, 15);
-    EmbossFitlerWrapper embossFitlerWrapper;
-    LensFitlerWrapper lensFilterWrapper(3);
-
-    GenericFilterWrapper *wrappers[] = {
-        &medianFilterWrapper, &boxFilterWrapper,   &gaussianFilterWrapper, &bilateralFilterWrapper, &erodeFilterWrapper,
-        &dilateFilterWrapper, &sobelFilterWrapper, &cannyFitlerWrapper,    &embossFitlerWrapper,    &lensFilterWrapper,
-    };
-    int wrapperTotal = sizeof(wrappers) / sizeof(wrappers[0]);
-
-    bool keepGoing = true;
-
-    cv::namedWindow(WINDOW_NAME, cv::WINDOW_NORMAL | cv::WINDOW_FREERATIO | cv::WINDOW_GUI_EXPANDED);
-    cv::resizeWindow(WINDOW_NAME, 1920, 1080);
-    cv::setMouseCallback(WINDOW_NAME, CallBackFunc);
-    do {
-        if (!freezeFrame) {
-            cap.read(inframe);
-
-            if (inframe.empty()) {
-                std::cerr << "ERROR! blank frame grabbed\n";
-                return -1;
-            }
-        }
-
-        cv::Mat outframe = inframe.clone();
-        wrappers[wrapperIndex]->applyFilter(outframe);
-        cv::resize(outframe, outframe, cv::Size(1240, 1080));
-        cv::imshow(WINDOW_NAME, outframe);
-        if (cv::waitKey(5) == ' ') {
-            wrapperIndex++;
-        }
-        if (wrapperIndex >= 3) {
-            keepGoing = false;
-        }
-    } while (keepGoing);
-    return 0;
+    QApplication app(argc, argv);
+    QLabel hello("<center>Welcome to my first Qt program</center>");
+    hello.setWindowTitle("My First Qt Program");
+    hello.resize(400, 400);
+    hello.show();
+    return app.exec();
 }

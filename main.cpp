beca73dcbad701486d66f88c89e4c8c7e3e71441
Victor TOPORAN <victor.toporan@gmail.com>
2023-04-01 19:25:19 +0300

    Moves window setup to separate class

--- main.cpp
+++ main.cpp
@@ -10,39 +10,11 @@
 #include "medianFilter.hpp"
 #include "sobelFilter.hpp"
 
-#include <cstdlib>
-#include <opencv2/highgui.hpp>
-#include <opencv2/opencv.hpp>
-
-#include <QApplication>
-#include <QGraphicsScene>
-#include <QLabel>
-#include <QMainWindow>
-#include <QPixmap>
-#include <QTimer>
-#include <QWidget>
-
-#include <qwindowdefs.h>
-#include <stdio.h>
-#include <string>
+#include "window.hpp"
 
 #define DEVICE_ID (0)
 #define API_ID (cv::CAP_ANY)
 #define WINDOW_NAME ("ISP Demo")
-
-cv::VideoCapture cap;
-
-void openCapture() {
-    int deviceID = 0;
-    int apiID = cv::CAP_ANY;
-    int ddepth = -1;
-
-    cap.open(deviceID, apiID);
-    if (!cap.isOpened()) {
-        std::cerr << "ERROR! Unable to open camera\n";
-        exit(-1);
-    }
-}
 
 int wrapperIndex = 5;
 int freezeFrame = false;
@@ -52,49 +24,9 @@ void CallBackFunc(int event, int x, int y, int flags, void *userdata) {
     }
 }
 
-class MainWindow : QMainWindow {
-        Q_OBJECT
-    public:
-        MainWindow() {}
-
-        ~MainWindow() {}
-        void start() {
-            QTimer *timer = new QTimer(this);
-            connect(timer, SIGNAL(timeout()), this, SLOT(Update()));
-            timer->start(20);
-        }
-
-    public slots:
-        void Update();
-
-    private:
-        cv::VideoCapture videoCap;
-        cv::Mat liveImage;
-        QImage image;
-        QLabel *label = new QLabel;
-};
-
-void MainWindow::Update() {
-    cv::Mat frame;
-    cap.read(frame);
-    if (frame.empty()) {
-        std::cerr << "ERROR! blank frame grabbed\n";
-        exit(1);
-    }
-    LensFitlerWrapper lensFilter = LensFitlerWrapper(-0.4);
-    lensFilter.applyFilter(frame);
-
-    image = QImage(frame.data, frame.cols, frame.rows, QImage::Format_RGB888).rgbSwapped();
-    label->setPixmap(QPixmap::fromImage(image));
-
-    label->show();
-}
-
 int main(int argc, char *argv[]) {
-    openCapture();
     QApplication app(argc, argv);
     MainWindow *window = new MainWindow();
-    window->start();
     return app.exec();
 }
 

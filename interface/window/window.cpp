bd93f8b8ba0a4ed9687412054a0df5b1f47faa9a
Victor TOPORAN <victor.toporan@gmail.com>
2023-04-15 18:40:38 +0300

    Adds improved conversion function of cv::Mat to QImage

--- interface/window/window.cpp
+++ interface/window/window.cpp
@@ -5,6 +5,38 @@ MainWindow::MainWindow() {
     startTimer();
     label = new QLabel;
     layer = new Layer(8);
+// https://asmaloney.com/2013/11/code/converting-between-cvmat-and-qimage-or-qpixmap
+static inline QImage cvMatToQImage(const cv::Mat &inMat) {
+    switch (inMat.type()) {
+    // 8-bit, 4 channel
+    case CV_8UC4: {
+        QImage image(inMat.data, inMat.cols, inMat.rows, static_cast<int>(inMat.step), QImage::Format_ARGB32);
+        return image;
+    }
+    // 8-bit, 3 channel
+    case CV_8UC3: {
+        QImage image(inMat.data, inMat.cols, inMat.rows, static_cast<int>(inMat.step), QImage::Format_RGB888);
+        return image.rgbSwapped();
+    }
+    // 8-bit, 1 channel
+    case CV_8UC1: {
+        static QVector<QRgb> sColorTable;
+        // only create our color table the first time
+        if (sColorTable.isEmpty()) {
+            sColorTable.resize(256);
+
+            for (int i = 0; i < 256; ++i) {
+                sColorTable[i] = qRgb(i, i, i);
+            }
+        }
+        QImage image(inMat.data, inMat.cols, inMat.rows, static_cast<int>(inMat.step), QImage::Format_Indexed8);
+        image.setColorTable(sColorTable);
+        return image;
+    }
+    default:
+        break;
+    }
+    return QImage();
 }
 
 void MainWindow::openCapture() {

70b0fecf86898f5b9e7b22256eb3c9135e5b4067
Victor TOPORAN <victor.toporan@gmail.com>
2023-05-28 22:32:26 +0300

    Adds dependencies and install scripts

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cmake -H. -Bbuild
+cd build
+make all
+
+cd ../
+DESKTOP_SHORTCUT=$HOME/Desktop/ISP_Demo.desktop
+
+echo "
+[Desktop Entry]
+Name=ISP Demo
+Exec=$(pwd)/build/IspDemo.exe
+Icon=$(pwd)/resources/ISP_Demo_Shutter.png
+Type=Application
+Terminal=false
+Encoding=UTF-8
+" > $DESKTOP_SHORTCUT
